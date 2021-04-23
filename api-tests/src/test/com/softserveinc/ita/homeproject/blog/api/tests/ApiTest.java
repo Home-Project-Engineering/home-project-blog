316326a855c8ec18cfb4272eb776f795f28a36ad
Oleksandr Khomenko <khomenko.dp@gmail.com>
2021-04-23 14:18:12 +0300

    Init API tests module.

---
+++ api-tests/src/test/com/softserveinc/ita/homeproject/blog/api/tests/ApiTest.java
@@ -0,0 +1,12 @@
+package com.softserveinc.ita.homeproject.blog.api.tests;
+
+import com.softserveinc.ita.homeproject.blog.ApiClient;
+import org.junit.jupiter.api.Test;
+
+class ApiTest {
+
+    @Test
+    void firstTest() {
+
+    }
+}
