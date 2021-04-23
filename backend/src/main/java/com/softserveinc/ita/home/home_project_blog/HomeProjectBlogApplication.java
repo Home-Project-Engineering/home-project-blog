06526583093edb0a9dbc59e500b1973ae6c07c8b
lidia <lidia.kalakutskaya@gmail.com>
2021-04-23 17:28:23 +0300

    Added spring boot security

--- backend/src/main/java/com/softserveinc/ita/home/home_project_blog/HomeProjectBlogApplication.java
+++ backend/src/main/java/com/softserveinc/ita/home/home_project_blog/HomeProjectBlogApplication.java
@@ -2,8 +2,9 @@ package com.softserveinc.ita.home.home_project_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HomeProjectBlogApplication {
 
 	public static void main(String[] args) {
