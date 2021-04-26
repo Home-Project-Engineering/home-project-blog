d9e1b7d52e5d166afefddf22ade42f17d555b40b
lidia <lidia.kalakutskaya@gmail.com>
2021-04-26 20:49:26 +0300

    Encripted passwords for users.

--- backend/src/main/resources/data-postgres.sql
+++ backend/src/main/resources/data-postgres.sql
@@ -1,9 +1,9 @@
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Lida', 'Lidia', 'Kalakutskaya', 'lia@gmail.com', 'jkl;213lkj', 'BLOGGER');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Lena', 'Elena', 'Grigorenko', 'lena@gmail.com', 'jk;lj321321l', 'MODERATOR');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Alena', 'Olena', 'Chepurnaya', 'alena@gmail.com', 'jkl;lk432j', 'MODERATOR');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Katya', 'Ekaterina', 'Glovatskaya', 'katya@gmail.com', 'jkl23;lkj', 'BLOGGER');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Roma', 'Roman', 'Kandibur', 'roma@gmail.com', 'jkl;lk32j', 'BLOGGER');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Max', 'Maxim', 'Risenko', 'max@gmail.com', 'jkl;lk32j', 'MODERATOR');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Sasha', 'Alexandr', 'Zvenigorodskiy', 'sanya@gmail.com', 'j121kl;lkj', 'BLOGGER');
-INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Oleg', 'Oleg', 'Miheev', 'oleg@gmail.com', '11jkl;lkj', 'ADMIN');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Lida', 'Lidia', 'Kalakutskaya', 'lia@gmail.com', 'TGlkYUxpZGE=', 'BLOGGER');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Lena', 'Elena', 'Grigorenko', 'lena@gmail.com', 'TGVuYUxlbmE=', 'MODERATOR');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Alena', 'Olena', 'Chepurnaya', 'alena@gmail.com', 'QWxlbmFBbGVuYQ==', 'MODERATOR');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Katya', 'Ekaterina', 'Glovatskaya', 'katya@gmail.com', 'S2F0eWFLYXR5YQ==', 'BLOGGER');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Roma', 'Roman', 'Kandibur', 'roma@gmail.com', 'Um9tYVJvbWE=', 'BLOGGER');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Max', 'Maxim', 'Risenko', 'max@gmail.com', 'TWF4TWF4TWF4', 'MODERATOR');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Sasha', 'Alexandr', 'Zvenigorodskiy', 'sanya@gmail.com', 'U2FzaGFTYXNoYQ==', 'BLOGGER');
+INSERT INTO users(name, firstName, lastName, email, password, role) VALUES('Oleg', 'Oleg', 'Miheev', 'oleg@gmail.com', 'T2xlZ09sZWc=', 'ADMIN');
 --     ADMIN, MODERATOR, BLOGGER, ANY
