dc432019e2643a0bb67d91f1caf929a8dadb22d0
lidia <lidia.kalakutskaya@gmail.com>
2021-04-26 22:30:09 +0300

    Deleted comments.

--- backend/src/main/java/com/softserveinc/ita/home/home_project_blog/configuration/BasicConfiguration.java
+++ backend/src/main/java/com/softserveinc/ita/home/home_project_blog/configuration/BasicConfiguration.java
@@ -1,6 +1,5 @@
 package com.softserveinc.ita.home.home_project_blog.configuration;
 
-import com.softserveinc.ita.home.home_project_blog.repository.entity.Role;
 import com.softserveinc.ita.home.home_project_blog.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -60,61 +56,4 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
     }
-
-
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        return new InMemoryUserDetailsManager(
-//                User.builder()
-//                        .username("admin")
-//                        .password(passwordEncoder().encode("admin"))
-//                        .authorities(Role.ADMIN.getAuthorities())
-//                        .build(),
-//                User.builder()
-//                        .username("user")
-//                        .password(passwordEncoder().encode("user"))
-//                        .authorities(Role.ANY.getAuthorities())
-//                        .build(),
-//                User.builder()
-//                        .username("blogger")
-//                        .password(passwordEncoder().encode("blogger"))
-//                        .authorities(Role.BLOGGER.getAuthorities())
-//                        .build(),
-//                User.builder()
-//                        .username("moderator")
-//                        .password(passwordEncoder().encode("moderator"))
-//                        .authorities(Role.MODERATOR.getAuthorities())
-//                        .build()
-//        );
-//    }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        PasswordEncoder encoder =
-//                PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//
-//        HttpSecurity http;
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-//                .antMatchers(HttpMethod.POST,"/api/**").hasRole(Role.ADMIN.name())
-//                .antMatchers(HttpMethod.DELETE,"/api/**").hasAnyRole(Role.ADMIN.name())
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-//
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user")
-//                .password(encoder.encode("password"))
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password(encoder.encode("admin"))
-//                .roles("USER", "ADMIN");
-//    }
 }
