fecdcc1f536b21ea8872fd5a2a81e61831fc2249
Vladyslav-Frolov <tertey7@gmail.com>
2021-04-28 14:45:24 +0300

    refactor simple test for user

--- backend/src/test/java/com/softserveinc/ita/homeprojectblog/controller/UserControllerTest.java
+++ backend/src/test/java/com/softserveinc/ita/homeprojectblog/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.softserveinc.ita.homeprojectblog.controller;
 
 import com.softserveinc.ita.homeprojectblog.generated.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.event.annotation.AfterTestMethod;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -11,28 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserControllerTest {
 
     @Test
-    void getUser() {
-        RestTemplate restTemplate = new RestTemplate();
-        User user = restTemplate.getForObject("http://localhost:8080/api/1/users/3", User.class);
-//        System.out.println(user);
-
+    public void createUser() {
+        User user;
         User testUser = new User();
-        testUser.setId(new BigDecimal(3));
-        testUser.setName("John78new2");
-        testUser.setFirstName("John");
-        testUser.setLastName("Smith");
-        testUser.setEmail("john.smith2@example.com");
-        testUser.setPassword("*****");
-        testUser.setRole(User.RoleEnum.ADMIN);
-
-        assertEquals(user, testUser);
-    }
-
-
-    @Test
-    void createUser() {
         RestTemplate restTemplate = new RestTemplate();
-        User testUser = new User();
         testUser.setName("John78new2111111");
         testUser.setFirstName("John");
         testUser.setLastName("Smith");
@@ -40,14 +24,36 @@ class UserControllerTest {
         testUser.setPassword("asdfasdf44444AAAAA");
         testUser.setRole(User.RoleEnum.ADMIN);
 
-        User user = restTemplate.postForObject("http://localhost:8080/api/1/users", testUser, User.class);
+        user = restTemplate.postForObject("http://localhost:8080/api/1/users", testUser, User.class);
         System.out.println(user);
         testUser.setPassword("*****");
         assert user != null;
         BigDecimal id = user.getId();
         testUser.setId(id);
-        restTemplate.delete("http://localhost:8080/api/1/users/" + id);
 
+        restTemplate.delete("http://localhost:8080/api/1/users/" + user.getId());
+
+        assertEquals(testUser, user);
+    }
+
+    @Test
+    void getUser() {
+        User tempUser = new User();
+        RestTemplate restTemplate = new RestTemplate();
+        tempUser.setName("John78new2111111");
+        tempUser.setFirstName("John");
+        tempUser.setLastName("Smith");
+        tempUser.setEmail("john.smith211111@example.com");
+        tempUser.setPassword("asdfasdf44444AAAAA");
+        tempUser.setRole(User.RoleEnum.ADMIN);
+
+        User user = restTemplate.postForObject("http://localhost:8080/api/1/users", tempUser, User.class);
+        assert user != null;
+        BigDecimal id = user.getId();
+
+        User testUser = restTemplate.getForObject("http://localhost:8080/api/1/users/" + id, User.class);
+        restTemplate.delete("http://localhost:8080/api/1/users/" + id);
         assertEquals(user, testUser);
     }
+
 }
