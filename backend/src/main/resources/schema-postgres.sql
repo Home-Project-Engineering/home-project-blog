72d18414ee0c327a6219b509a2bc9887824e4e4d
lidia <lidia.kalakutskaya@gmail.com>
2021-04-28 19:41:19 +0300

    Added schemas posts and tags tables

--- backend/src/main/resources/schema-postgres.sql
+++ backend/src/main/resources/schema-postgres.sql
@@ -10,16 +10,22 @@ CREATE TABLE users
     role      varchar(10)           NOT NULL
 );
 
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags
+(
+    id   bigserial PRIMARY KEY NOT NULL,
+    name VARCHAR(255)          NOT NULL UNIQUE
+)
+
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts
 (
-    post_id   bigserial PRIMARY KEY NOT NULL,
-    text      VARCHAR(255)          NOT NULL UNIQUE,
-    title     VARCHAR(255),
-    previewAttachment  VARCHAR(255),
-    email     VARCHAR(255)          NOT NULL UNIQUE,
-    password  VARCHAR(255)          NOT NULL,
-    role      varchar(10)           NOT NULL
+    id                bigserial PRIMARY KEY NOT NULL,
+    text              VARCHAR,
+    title             VARCHAR(255),
+    previewAttachment VARCHAR(255),
+--     user_id bigserial FOREIGN KEY REFERENCES blog.users,
+--     tag_id bigserial FOREIGN KEY REFERENCES blog.tag
 );
 
 -- CREATE TYPE user_role AS ENUM ( 'guest', 'user', 'moderator', 'admin', 'expert');
