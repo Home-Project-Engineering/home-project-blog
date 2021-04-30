ffb8e79daa166dae3ca7224410b762e94b1b480d
lidia <lidia.kalakutskaya@gmail.com>
2021-04-30 05:52:06 +0300

    Optimized

--- backend/src/main/java/com/softserveinc/ita/home/home_project_blog/HomeProjectBlogApplication.java
+++ backend/src/main/java/com/softserveinc/ita/home/home_project_blog/HomeProjectBlogApplication.java
@@ -2,13 +2,12 @@ package com.softserveinc.ita.home.home_project_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class HomeProjectBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeProjectBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeProjectBlogApplication.class, args);
+    }
 
 }
