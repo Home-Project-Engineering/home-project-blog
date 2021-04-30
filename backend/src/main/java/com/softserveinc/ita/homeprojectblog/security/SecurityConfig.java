61200fc2160bce99a66b82b34bef033a9d7f2e1f
Vladyslav-Frolov <tertey7@gmail.com>
2021-04-30 17:55:50 +0300

    redesigned project structure / add pom.xml for main module / boot parent to main module

--- backend/src/main/java/com/softserveinc/ita/homeprojectblog/security/SecurityConfig.java
+++ backend/src/main/java/com/softserveinc/ita/homeprojectblog/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.softserveinc.ita.homeprojectblog.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 
 @Configuration
 @EnableWebSecurity
