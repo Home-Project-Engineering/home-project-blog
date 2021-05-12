ab6377b4e5e9f874337b3f0169c9d182ea802fdc
Vladyslav-Frolov <tertey7@gmail.com>
2021-05-12 11:40:50 +0300

    delete basic realization

--- backend/src/main/resources/database/populateDB.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "role" (name) VALUES ('ADMIN');
-INSERT INTO "role" (name) VALUES ('MODERATOR');
-INSERT INTO "role" (name) VALUES ('BLOGGER');
-
-INSERT INTO "user" (name, first_name, last_name, email, password, role_id) VALUES ('Tertey', 'Vladyslav', 'Frolov', 'tertey7@mail.com', '$2a$12$jnDONqLCCjSIz3tbG6SS.uLRX4wdpxWw2V23JRr0ludGcWWuz4phS', '1');
-INSERT INTO "user" (name, first_name, last_name, email, password, role_id) VALUES ('Proselit', 'Evhenii', 'Suleimanov', 'proselit@mail.com', '$2a$12$jnDONqLCCjSIz3tbG6SS.uLRX4wdpxWw2V23JRr0ludGcWWuz4phS', '2');
-INSERT INTO "user" (name, first_name, last_name, email, password, role_id) VALUES ('CheGevara', 'Roman', 'Mohyla', 'chegevara@mail.com', '$2a$12$jnDONqLCCjSIz3tbG6SS.uLRX4wdpxWw2V23JRr0ludGcWWuz4phS', '3');
