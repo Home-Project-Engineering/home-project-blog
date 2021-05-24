db3b95cb7f88f320fedc0e0dad7cf2bcd1b55e1f
vmitianin <valerii.dnepr@gmail.com>
2021-05-24 20:17:30 +0300

    fix pom & add dependency

--- backend/src/main/java/com/example/blog/util/security/config/SecurityConfig.java
+++ backend/src/main/java/com/example/blog/util/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.blog.backend.util.security.config;
+package com.example.blog.util.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
