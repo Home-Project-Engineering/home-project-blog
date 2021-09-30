36b967729cc4fd42bc0a07bdc8cb72bcf9d71f11
lawalina <alina.lawrentyeva@gmail.com>
2021-09-30 08:36:27 +0300

    Added Spring Boot

---
+++ backend/src/main/java/com/homeproject/blog/backend/HomeProjectBlogApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.homeproject.blog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProjectBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeProjectBlogApplication.class, args);
+	}
+
+}
