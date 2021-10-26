128a754901b762c544dadb2af32b08ba3a688b59
Dmitry Varhanov <varhanovdm@gmail.com>
2021-10-26 18:56:06 +0300

    updates

--- backend/src/main/java/com/homeproject/blog/backend/exceptions/handler/ControllerExceptionHandler.java
+++ backend/src/main/java/com/homeproject/blog/backend/exceptions/handler/ControllerExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.homeproject.blog.backend.exceptions.handler;
 
-import com.homeproject.blog.backend.businesslayer.dto.ErrorDTO;
 import com.homeproject.blog.backend.exceptions.*;
+import com.homeproject.blog.backend.presentationlayer.model.Error;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -12,7 +13,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(value
             = {CommentNotFoundException.class, ForbiddenActionException.class,
             PostNotFoundException.class, TagNotFoundException.class, InternalServerErrorException.class})
-    protected ResponseEntity<Object> handleConflict(BlogException ex) {
-        return new ResponseEntity<>(new ErrorDTO(ex.getCode(), ex.getMessage()), ex.getHttpStatus());
+    protected ResponseEntity<Error> handleConflict(BlogException ex) {
+        Error error = new Error();
+        error.setCode(ex.getCode());
+        error.setMessage(ex.getMessage());
+        return new ResponseEntity<>(error, ex.getHttpStatus());
     }
+//    @ExceptionHandler
+//    protected ResponseEntity<Error> handleMismatchException(MethodArgumentTypeMismatchException ex){
+//
+//        return null;
+//    }
 }
--- backend/src/main/java/com/homeproject/blog/backend/presentationlayer/controllers/PostController.java
+++ backend/src/main/java/com/homeproject/blog/backend/presentationlayer/controllers/PostController.java
@@ -1,8 +1,11 @@
 package com.homeproject.blog.backend.presentationlayer.controllers;
 
+import com.homeproject.blog.backend.businesslayer.dto.CommentDTO;
 import com.homeproject.blog.backend.businesslayer.dto.PostDTO;
+import com.homeproject.blog.backend.businesslayer.services.CommentService;
 import com.homeproject.blog.backend.businesslayer.services.PostService;
 import com.homeproject.blog.backend.presentationlayer.config.ParametersConfig;
+import com.homeproject.blog.backend.presentationlayer.model.Comment;
 import com.homeproject.blog.backend.presentationlayer.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
@@ -23,6 +26,9 @@ public class PostController implements PostsApi{
     private PostService postService;
 
     @Autowired
+    private CommentService commentService;
+
+    @Autowired
     private ConversionService conversionService;
 
     @Override
@@ -32,14 +38,31 @@ public class PostController implements PostsApi{
     }
 
     @Override
+    public ResponseEntity<Comment> createComment(Long postId, Comment comment){
+        CommentDTO commentDTO = commentService.createComment(postId, conversionService.convert(comment, CommentDTO.class));
+        return ResponseEntity.status(HttpStatus.CREATED).body(conversionService.convert(commentDTO, Comment.class));
+    }
+
+    @Override
     public ResponseEntity<List<Post>> getPosts(Long id, Long tagId, String tagName, String authorName,  String sort, Integer pageNum, Integer pageSize) {
         Page<PostDTO> page = postService.getPosts(id, tagId, tagName, authorName, ParametersConfig.getSortParameters(pageNum, pageSize, sort));
         return ResponseEntity.ok().header("X-Total-Count", String.valueOf(page.getTotalElements())).body(page.stream().map(post -> conversionService.convert(post, Post.class)).collect(Collectors.toList()));
     }
 
     @Override
+    public ResponseEntity<List<Comment>> getComments(Long postId, Long id, String authorName, String sort, Integer pageNum, Integer pageSize) {
+        Page<CommentDTO> page = commentService.getComments(postId, id, authorName, ParametersConfig.getSortParameters(pageNum, pageSize, sort));
+        return ResponseEntity.ok().header("X-Total-Count", String.valueOf(page.getTotalElements())).body(page.stream().map(comment -> conversionService.convert(comment, Comment.class)).collect(Collectors.toList()));
+    }
+
+    @Override
     public ResponseEntity<Post> getPost(Long id){
         return ResponseEntity.ok(conversionService.convert(postService.readPost(id), Post.class));
+    }
+
+    @Override
+    public ResponseEntity<Comment> getComment(Long postId, Long id){
+        return ResponseEntity.ok(conversionService.convert(commentService.readComment(postId, id), Comment.class));
     }
 
     @Override
@@ -49,8 +72,20 @@ public class PostController implements PostsApi{
     }
 
     @Override
+    public ResponseEntity<Comment> updateComment(Long postId, Long id, Comment comment) {
+        CommentDTO commentDTO = commentService.updateComment(postId, id, conversionService.convert(comment, CommentDTO.class));
+        return ResponseEntity.ok(conversionService.convert(commentDTO, Comment.class));
+    }
+
+    @Override
     public ResponseEntity<Void> removePost(Long id) {
         postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<Void> removeComment(Long postId, Long id) {
+        commentService.deleteComment(postId, id);
         return ResponseEntity.noContent().build();
     }
 }
